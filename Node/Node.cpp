59651543040ddc9b7f389860edf2a139dd70a5ee
Nicko Cruz M <nickocruzm@gmail.com>
2022-08-19 20:02:09 -0700

    Removed the defined constructor with the next attribute

--- Node/Node.cpp
+++ Node/Node.cpp
@@ -10,10 +10,7 @@ Node::Node(int value){
     this->value = value;
     this->next = nullptr;
 }
-Node::Node(int value, Node* next){
-    this->value = value;
-    this->next = next;
-}
+
 
 int Node::getValue(){
     return this->value;
