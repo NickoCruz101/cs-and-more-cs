c4a4f628a0b4dd1b550df71947d5504583f9f0a6
Nicko Cruz M <nickocruzm@gmail.com>
2022-08-28 20:12:17 -0700

    small fixes

--- Node/Node.cpp
+++ Node/Node.cpp
@@ -2,9 +2,12 @@
 #include "Node.h"
 
 
-Node::Node(){
-    this->value = NULL;
+Node::Node(){}
+
+Node::Node(int value){
+    this->value = value;
 }
+
 Node::Node(int value,Node* next){
     this->value = value;
     this->next = next;
--- Node/Node.h
+++ Node/Node.h
@@ -1,13 +1,15 @@
-#ifndef NODE_H
-#define NODE_H
+#ifndef __NODE_H__
+#define __NODE_H__
 
+#define NULL 0
 class Node{
 
     public:
-        int value;
-        Node* next;
+        int value = NULL;
+        Node* next = nullptr;
         
         Node();
+        Node(int value);
         Node(int value, Node* next);
 
         int getValue();
