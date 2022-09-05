723d30ba14901fb095a2ca79727a05982096c965
Nicko Cruz M <nickocruzm@gmail.com>
2022-09-04 22:46:23 -0700

    Deleted Build directory

--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nickocruz/Developer/GitHub/cs-and-more-cs/googletest
-# Build directory: /Users/nickocruz/Developer/GitHub/cs-and-more-cs/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
