7c2fcc8c1b42caf12b3650ba876cc595cfbedf29
Nicko Cruz M <nickocruzm@gmail.com>
2022-09-04 22:58:35 -0700

    no arguments test, test name change

--- Testing.h
+++ Testing.h
@@ -5,21 +5,28 @@
 #include "Linked_List/LinkedList.h"
 #include "gtest/gtest.h"
 
-TEST(NODE_CONSTRUCTOR_TEST, 1_PARAMS){
+TEST(NODE_CONSTRUCTOR_TEST, no_arguments){
+    Node* n = new Node();
+
+    EXPECT_EQ(n->getValue(),0);
+    EXPECT_EQ(n->next,nullptr);
+}
+
+TEST(NODE_CONSTRUCTOR_TEST, val_argument){
     Node* n = new Node(10);
 
     EXPECT_EQ(10,n->getValue());
 
 };
 
-TEST(NODE_CONSTRUCTOR_TEST, 2_PARMS){
+TEST(NODE_CONSTRUCTOR_TEST, val_and_Node_arguments){
     Node* b = new Node(14);
     Node* a = new Node(22,b);
 
     EXPECT_EQ(a->next, b);
     EXPECT_EQ(a->getValue(),22);
     EXPECT_EQ(b->getValue(),14);
-}
+};
 
 
 
