7ee61399d06fcb0c14b84fdce38a8ffc0cc8088f
Nicko Cruz M <nickocruzm@gmail.com>
2022-09-04 22:52:39 -0700

    Test name change

--- Testing.h
+++ Testing.h
@@ -5,24 +5,20 @@
 #include "Linked_List/LinkedList.h"
 #include "gtest/gtest.h"
 
-TEST(NODE_CONSTRUCTOR_1, 1_PARAMS){
+TEST(NODE_CONSTRUCTOR_TEST, 1_PARAMS){
     Node* n = new Node(10);
 
     EXPECT_EQ(10,n->getValue());
 
 };
 
-TEST(NODE_CONSTRUCTOR_2, 2_PARMS){
+TEST(NODE_CONSTRUCTOR_TEST, 2_PARMS){
     Node* b = new Node(14);
     Node* a = new Node(22,b);
 
     EXPECT_EQ(a->next, b);
     EXPECT_EQ(a->getValue(),22);
     EXPECT_EQ(b->getValue(),14);
-
-    
-
-
 }
 
 
