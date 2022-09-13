6f8571aa5cd8616916b84efdb4c5d27709bd82ff
nickocruzm <nickocruzm@gmail.com>
2022-09-12 18:11:28 -0700

    Swapmethod implementation

--- Algorithms/BubbleSort.cpp
+++ Algorithms/BubbleSort.cpp
@@ -1,14 +1,23 @@
 #include "../Linked_List/LinkedList.h"
 #include "../Node/Node.h"
 
-
+// swap a with b
+void swap(Node* a, Node* b){
+    Node* tmp = b->next;
+    b->next = a;
+    a->next = tmp;
+}
 
 void BubbleSort(LinkedList* list){
     Node* a_ptr = list->getHead();
     Node* b_ptr = nullptr;
 
-    // Iterate through list
+    while(b_ptr->next != nullptr){
+        if(a_ptr->value > b_ptr->value){
+            swap(a_ptr,b_ptr);
+        }
 
-    //
+        
+    }
 
 }
