a8e4652fd0b6b8343002594876950fae564e7bcf
Nicko Cruz M <nickocruzm@gmail.com>
2022-09-16 14:05:46 -0700

    attempting to create seperate testing files

--- Testing/Testing.h
+++ Testing/Testing.h
@@ -1,7 +1,6 @@
 #ifndef __TESTING_H__
 #define __TESTING_H__
 
-#include "../Node/Node.h"
 #include "../Linked_List/LinkedList.h"
 #include "gtest/gtest.h"
 
@@ -11,50 +10,6 @@ Node* g_c {new Node(3)};
 Node* g_d {new Node(4)};
 Node* head {new Node(33)};
 LinkedList* test_list {new LinkedList(head)};
-
-TEST(NODE_CONSTRUCTOR_TEST, no_arguments){
-    Node* n = new Node();
-
-    EXPECT_EQ(n->getValue(),0);
-    EXPECT_EQ(n->next,nullptr);
-}
-
-TEST(NODE_CONSTRUCTOR_TEST, 1_argument){
-    Node* n = new Node(10);
-
-    EXPECT_EQ(10,n->getValue());
-
-};
-
-TEST(NODE_CONSTRUCTOR_TEST, 2_arguments){
-    Node* b = new Node(14);
-    Node* a = new Node(22,b);
-
-    EXPECT_EQ(a->next, b);
-    EXPECT_EQ(a->getValue(),22);
-    EXPECT_EQ(b->getValue(),14);
-};
-
-
-TEST(NODE_SETTERS, set_next){
-    Node* a = new Node(1);
-    Node* b = new Node(33);
-
-    a->set("next",b);
-
-    EXPECT_EQ(a->next, b);
-    EXPECT_EQ(a->next->value, b->getValue());
-};
-
-TEST(NODE_SETTERS, set_prev){
-    Node* prev = new Node(22);
-    Node* node = new Node(32);
-
-    node->set("prev",prev);
-
-    EXPECT_EQ(node->prev, prev);
-    EXPECT_EQ(node->prev->value, prev->value);
-}
 
 TEST(LINKEDLIST_CONSTRUCTORS, empty){
     LinkedList* list = new LinkedList();
