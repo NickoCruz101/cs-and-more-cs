8158a5488a5daaa81b6a46b6250d0282c327c2d4
Nicko Cruz M <nickocruzm@gmail.com>
2022-09-17 17:57:03 -0700

    reformat

--- Algorithms/BubbleSort.cpp
+++ Algorithms/BubbleSort.cpp
@@ -19,36 +19,11 @@ int main(){
     LinkedList* list = new LinkedList();
     list->create(vec);
 
-    list->print();
-
     bubbleSort(list);
-
-    list->print();
-
-
-
-    // bubbleSort(arr,6);
-
-    // for(int i = 0; i < 6; i++){
-    //     cout<< arr[i]<<endl;
-    // }
-
-
 }
 void bubbleSort(LinkedList* l){
     for(Node* i = l->getHead(); i != nullptr; i++){
-
-        for(Node* j = i->next; j != nullptr; j++){
-            
-            if(i->getValue() > j->getValue()){
-                Node* temp = j->next;
-                j->next = i;
-                i->next = temp;
-                
-            }else{
-                break;
-            }
-        }
+        cout << i << endl;
     }
 }
 
@@ -65,27 +40,3 @@ void bubbleSort(int arr[], int n){
         }
     }
 }
-
-// void bubble_sort(vector<int>& vec);
-// void swap(int a, int b);
-
-
-// void swap(int* a, int* b){
-
-// }
-
-
-
-// void bubble_sort(vector<int>& vec){
-//     int* a = nullptr;
-//     int* b = nullptr;
-
-//     for(int i = 1; i < vec.size(); i++){
-//         a = &vec[i-1];
-//         b = &vec[i]
-
-//         if( < vec[i-1]){
-//             swap(vec[i],vec[i-1]);
-//         }
-//     }
-// }
