e9db352c5c5716197851508369c1d5900d878d84
Nicko Cruz M <nickocruzm@gmail.com>
2022-09-18 22:17:39 -0700

    first try

---
+++ Stack/main.py
@@ -0,0 +1,11 @@
+import stack
+
+if(__name__ == '__main__'):
+    
+    stackA = stack.stack()
+    
+    stackA.push(22)
+    stackA.push(23)
+    stackA.push(24)
+    
+    stackA.describe()
---
+++ Stack/stack.py
@@ -0,0 +1,25 @@
+class stack:
+    def __init__(self):
+        self.__size = 0
+        self.arr = []
+    
+    def push(self,item):
+        index = self.__size
+        self.arr[index] = item
+        self.__size += 1
+        
+    def pop(self):
+        try:
+            del self.arr[-1]
+            self.__size -= 1
+        except IndexError:
+            print(self.__size, "INDEX ERROR")
+    
+    def describe(self):
+        for i in range(self.__size):
+            print(self.arr[i])
+    
+
+            
+        
+        
