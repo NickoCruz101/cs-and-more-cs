0cb5b87a106ec57977afa1ca97505ab725cb984f
Nicko Cruz M <nickocruzm@gmail.com>
2022-10-24 23:04:34 -0700

    init vector (not Global)

--- KnapSack/main.cpp
+++ KnapSack/main.cpp
@@ -7,27 +7,34 @@ using namespace std;
 
 struct item{
     int price,val;
-
     item(int p, int v):price(p),val(v) {};
-
 };
+
 
 vector<item> items; // Global items vector
 
+
+
 int main(){
-    int capacity, n; //capacity of bag and (n) lines to read in
+    int capacity,n; //capacity of bag and (n) lines to read in
     cin >>capacity >> n;
+
+    vector<int> S(capacity,0);
+
+    for(auto i: S){
+        cout << i << endl;
+    }
 
     int currLine = 1; // line counter
     // Reads in each line assigning them to an item object and pushing them back into
     // the items vector
 
-    int w,v;
-    while(currLine <= n){
-        cin>>w>>v;
-        item i(w,v);
-        items.push_back(i);
-        currLine = currLine + 1;
-    }
+    // int w,v;
+    // while(currLine <= n){
+    //     cin>>w>>v;
+    //     item i(w,v);
+    //     items.push_back(i);
+    //     currLine = currLine + 1;
+    // }
 
 }
