949d5a2f62851654ebad7cbc048ecad9c99739df
Nicko Cruz M <nickocruzm@gmail.com>
2022-11-06 15:12:51 -0800

    functional

--- LongDecSubSeq/main.cpp
+++ LongDecSubSeq/main.cpp
@@ -3,36 +3,57 @@
 
 using namespace std;
 
-int lds(int arr[], int n){
-    int l[n];
-    int i, j, max = 0;
-
-    for(i = 0; i < n; i++){
-        l[i] = 1;
+int lds(vector<int> &seq, int n)
+{
+    vector<int> L;
+    for (int p = 0; p < n; p++)
+    {
+        L.push_back(0);
     }
 
-    for(i = 1; i < n; i++){
-        for(j = 0; j < i; j++){
-            if(arr[i] < arr[j] && l[i] < l[j]){
-                l[i] = l[j] + 1;
+    
+    int best = 0;
+    for (int i = 0; i < n; i++)
+    {
+        best = L[i];
+        for (int j = 0; j < i; j++)
+        {
+            if (seq[i] < seq[j] && best < L[j])
+            {
+                best = L[j];
             }
         }
+        L[i] = best + 1;
     }
 
-    for(i = 0; i < n; i++){
-        if(max < l[i]){
-            max = l[i];
+    int max = 0;
+    for(int i = 0; i < L.size(); i++){
+        if(max < L[i]){
+            max = L[i];
         }
     }
 
+    // for(auto i: L){
+    //     cout << i << " ";
+    // }
+
     return max;
+
+
 }
 
 int main(){
+    int n1 = 9;
+    vector<int> seq1 = {1,5,4,7,9,6,3,8,2};
 
-    int arr[] = {1,5,4,7,9,6,3,8,2};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n2 = 5;
+    vector<int> seq2 = {22160, 297625, 230329, 191306, 867900};
 
-    cout<< lds(arr,n) << endl;
+    int n3 = 8;
+    vector<int> seq3 = {878361,973449,649581,186790,904723,54458,959645,322343};
+
+    cout<<lds(seq1,n1)<<endl;
+    cout<<lds(seq2,n2)<< endl;
+    cout<<lds(seq3,n3)<< endl;
 
 }
