74e195628f6e1ec822ca2faec0d18d7eca80f8bc
Nicko Cruz M <nickocruzm@gmail.com>
2022-11-10 01:39:48 -0800

    cleaned

--- WeightedEditDist/main.cpp
+++ WeightedEditDist/main.cpp
@@ -4,43 +4,11 @@
 
 using namespace std;
 
-class Seq{
-
-public:
-    int seq_size;
-    vector<int> seq;
-    Seq(){
-        this->seq_size = 0;
-        this->seq = {};
-    }
-    Seq(int length){
-        this->seq_size = length;
-        this->seq = seq;
-        int x;
-        for(int i = 0; i < this->seq_size;i++){
-            cin >> x;
-            this->seq.push_back(x);
-        }
-    }
-};
-
-class A: public Seq{
-    A(int l): Seq(l) {};
-};
-
-class B: public Seq{
-    B():Seq(0) {};
-    B(int l): Seq(l){};
-};
-
 int main(){
     //Get input
     int lenA,lenB;
 
     cin >> lenA, lenB;
-    
-    A(lenA);
-    B(lenB);
 
 }
 
