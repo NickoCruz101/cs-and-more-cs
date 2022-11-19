aa66608a9b0df512b00a4331ba92845913b58d5f
Nicko Cruz M <nickocruzm@gmail.com>
2022-11-18 20:13:32 -0800

    functional

--- ShortestPath/v1.cpp
+++ ShortestPath/v1.cpp
@@ -4,6 +4,43 @@
 
 using namespace std;
 
+
+
+class Graph{
+
+public:
+    vector<int> t_dist;
+
+    Graph(){
+        this->t_dist = {};
+    };
+
+};
+
 int main(){
+    int n; // number of vertices
+    int m; // number of edges
+    cin >> n >> m;
+
+    int s; // source vertex
+    int t; // ending vertex
+
+    cin >> s >> t;
+
+    Graph G;
+
+    int inf = numeric_limits<int>::max(); // infinity
+
+    // assigns tentative distances to infinity
+    // when i == s we assign that value to 0
+    for(int i = 0; i < n; i++){
+        G.t_dist.push_back(0); 
+    }
+
+
+
+
+
+
 
 }
