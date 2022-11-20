55cdce35c224e9e4736751f65ed0f6a69e30fd46
Nicko Cruz M <nickocruzm@gmail.com>
2022-11-19 18:44:25 -0800

    Getting the Neighbors is a struggle

---
+++ ShortestPath/DjikstraAlg.md
@@ -0,0 +1,9 @@
+# Dijkstra's Algorithm
+
+## Graph 
+
+Graph object acting as a container to hold all the information of the Graph which we are attempting to find the single shortest path to.
+
+## Priority Queue (Binary Heap)
+
+
--- ShortestPath/v1.cpp
+++ ShortestPath/v1.cpp
@@ -1,28 +1,76 @@
 #include <iostream>
 #include <limits>
 #include <vector>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
+struct edge{
+    int u;
+    int v;
+    int weight;
 
+    edge(int i, int f, int w){
+        u = i;
+        v = f;
+        weight = w;
+    }
+
+    int getWeight(){
+        return this->weight;
+    }
+
+    string output(){
+        ostringstream oss;
+        oss << "( " << this->u<< " -> "<< this->v<<" , "<< this->weight<< " )"<< endl;
+        string o = oss.str();
+        return o;
+    }
+
+
+};
 
 class Graph{
 
 public:
-    vector<int> t_dist;
+    vector<int> Tdist; // tentative distance, 
+                        // Tdist[k] is the distance assigned to kth vertex
+
+    vector<int> Q; // priority queue
+    vector<edge> E; // Set of Edges
+
 
     Graph(){
-        this->t_dist = {};
+        this->Tdist = {};
+        this->Q = {};
+        this->E = {};
     };
 
+    vector<edge> getNeighbors(int p){
+        vector<edge> N;
+        for(int i = 0; i < this->E.size(); i++){
+            if(E[i].u == p || E[i].v == p){
+                N.push_back(E[i]);
+            }
 
+        }
 
-    void output_t_dist(){
-        for(int i = 0; i < this->t_dist.size(); i++){
-            cout << t_dist[i] << ", ";
+        return N;
+    }
+
+    void output(){
+        for(int i = 0; i < this->Tdist.size(); i++){
+            cout << Tdist[i] << ", ";
         }
         cout << endl;
+
+        for(int j = 0; j < this->E.size(); j++){
+            cout<< E[j].output() << " ";
+        }
     }
+
+
 
 };
 
@@ -36,17 +84,38 @@ int main(){
 
     cin >> s >> t;
 
-    Graph G;
+    Graph G; // Graph init
 
     int inf = numeric_limits<int>::max(); // infinity
 
     // assigns tentative distances to infinity
     // when i == s we assign that value to 0
     for(int i = 0; i < n; i++){
-        G.t_dist.push_back(inf); 
+        G.Tdist.push_back(inf); 
+    }
+    G.Tdist[s] = 0; // source vertex
+
+    int vi;
+    int vf;
+    int weight;
+
+    for(int i = 0; i < m; i++){
+        cin>> vi>> vf>> weight; 
+        G.E.push_back(edge(vi,vf,weight));
     }
 
-    G.t_dist[s] = 0;
+    G.output();
+
+    vector<edge> N = G.getNeighbors(0);
+
+    for(int i = 0; N.size(); i++){
+        cout<< N[i].output();
+    }
+
+
+
+
+
 
 
 
