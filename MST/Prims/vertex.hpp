d276fd6645f416335cebc751360015bd088d5d26
Nicko Cruz M <nickocruzm@gmail.com>
2022-11-21 23:49:39 -0800

    prim failure

---
+++ MST/Prims/vertex.hpp
@@ -0,0 +1,21 @@
+#ifndef __VERTEX_HPP__
+#define __VERTEX_HPP__
+
+class vertex{
+private:
+
+public:
+    vector<int> N;
+    int v;
+    bool visited;
+    vertex(){
+        this->v = -1;
+        this->N = {};
+        this->visited = false;
+    }
+    void assignNeight(int)
+
+
+}
+
+#endif
