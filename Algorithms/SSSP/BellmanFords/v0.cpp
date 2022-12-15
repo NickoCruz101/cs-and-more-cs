d99f79865a847f05d85611bc12bdbf3f50a65bb5
Nicko Cruz M <nickocruzm@gmail.com>
2022-12-15 15:42:19 -0800

    movedd

--- Algorithms/SSSP/BellmanFords/v0.cpp
+++ Algorithms/SSSP/BellmanFords/v0.cpp
@@ -45,24 +45,34 @@ int inf = numeric_limits<int>::max();
 
 int main(){
 
+    int n = 5;
+    int m = 7;
+    //int n, m;
+    //cin>> n>> m;
 
-    int n, m;
-    cin>> n>> m;
+    //int start, dest;
+    //cin>> start>> dest;
 
-    int start, dest;
-    cin>> start>> dest;
-
+    int start = 4;
+    int dest = 3;
 
     Graph G(n);
 
-    int u,v,w;
-    for(int i = 0; i < m; i++){
-        cin >> u >> v >> w;
-        G.addEdge(u,v,w);
-    }
+    // int u,v,w;
+    // for(int i = 0; i < m; i++){
+    //     cin >> u >> v >> w;
+    //     G.addEdge(u,v,w);
+    // }
+
+    G.addEdge(0,1,78);
+    G.addEdge(1,2,94);
+    G.addEdge(1,3,63);
+    G.addEdge(2,4,49);
+    G.addEdge(0,2,34);
+    G.addEdge(3,4,686);
+    G.addEdge(0,3,904);
 
     BellmanFord(G,start,dest);
-    
     
 }
 
@@ -70,13 +80,35 @@ int BellmanFord(Graph& G, int s, int f){
     int N = G.v_count();
     int M = G.e_count();
     vector<int> T_dist(N,inf);
-    if(f<s){
-        int t = s;
-        s = f;
-        f = t;
+    // if(f<s){
+    //     int t = s;
+    //     s = f;
+    //     f = t;
+
+    // }
+    T_dist[s] = 0;
+
+    int su = s;
+    for(int i = 0; i < N; i++){
+        int ui = G.E[i].u;
+        int vi = G.E[i].v;
+
+        if(su != ui){
+            continue;
+        }
+
+        else{ 
+            T_dist[vi] = T_dist[ui] + G.E[i].w;
+        }
+
 
     }
-    T_dist[s] = 0;
+
+
+    cout<< endl;
+    for(int i = 0; i < T_dist.size(); i++){
+        cout<< "i: "<<i << " "<< T_dist[i] << endl;
+    }
 
     for(int i = 0; i < N; i++){
         cout<<"i: "<< i << endl;
@@ -95,17 +127,19 @@ int BellmanFord(Graph& G, int s, int f){
                 T_dist[bv] = T_dist[bu] + bw;
             }
 
+
+
         }
 
-        for (int i = 0; i < G.e_count(); i++) {
-            int bu = G.E[i].u;
-            int bv = G.E[i].v;
-            int weight = G.E[i].w;
-            if (T_dist[bu] != inf
-                && T_dist[bu] + weight < T_dist[bv]) {
-            }
+        // for (int i = 0; i < G.e_count(); i++) {
+        //     int bu = G.E[i].u;
+        //     int bv = G.E[i].v;
+        //     int weight = G.E[i].w;
+        //     if (T_dist[bu] != inf
+        //         && T_dist[bu] + weight < T_dist[bv]) {
+        //     }
  
-        }
+        // }
     }
     cout<< endl;
     for(int i = 0; i < T_dist.size(); i++){
--- Random/defineDir.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#define multiply(f1,f2) (f1 * f2)
-
-using namespace std;
-
-int main(){
-    cout << multiply(4,3);
-}
